2500ad7f9996863ddc2786901faa892afd9fe226
vaishnavi padhar <68491981+maicodee@users.noreply.github.com>
2023-07-22 10:56:20 +0530

    Code Cleaned

--- src/classes/List.java
+++ src/classes/List.java
@@ -1,32 +1,53 @@
 package classes;
 
-import java.awt.GridLayout;
-import java.awt.Color;
+
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
-public class List extends JPanel {
-	
-	List(){
+public class List extends JPanel{
+
+	List()
+	{
+		
 		GridLayout layout = new GridLayout(10,1);
 		layout.setVgap(5);
 		
-	    //this.setBackground(Color.blue);
 		this.setLayout(layout);
+		this.setPreferredSize(new Dimension(400,560));
+		
 	}
 	
-	public void updateNumbers() {
+	public void updateNumbers()
+	{
 		Component[] listItems = this.getComponents();
 		
-		for(int i = 0 ;i < listItems.length; i++) {
-			
+		for(int i = 0;i<listItems.length;i++)
+		{
 			if(listItems[i] instanceof Task)
 			{
 				((Task)listItems[i]).changeIndex(i+1);
 			}
-			
 		}
+		
 	}
-
+	
+	public void removeCompletedTasks()
+	{
+		
+		for(Component c : getComponents())
+		{
+			if(c instanceof Task)
+			{
+				if(((Task)c).getState())
+				{
+					remove(c);
+					updateNumbers();
+				}
+			}
+		}
+		
+	}
 }
