c6c32db9040e8e69086478d7a2852ab7d7c76f27
vaishnavi padhar <68491981+maicodee@users.noreply.github.com>
2023-07-22 10:55:49 +0530

    Code Cleaned

--- src/classes/AppFrame.java
+++ src/classes/AppFrame.java
@@ -7,67 +7,75 @@ import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-import javax.swing.JFrame;
 
-public class AppFrame extends JFrame  {
+public class AppFrame extends JFrame{
 	
 	private TitleBar title;
+	private Footer footer;
 	private List list;
-	private ButtonPanel btnPanel;
 	
-	private JButton addTask;
+	private JButton newTask;
 	private JButton clear;
-	
-	//constructor
 	
 	AppFrame()
 	{
-		this.setSize(400 ,700);
+		this.setSize(400,700);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
-		title = new TitleBar();
-		list = new List();
-		btnPanel =  new ButtonPanel();
-		this.add(title,BorderLayout.NORTH);
-		this.add(btnPanel,BorderLayout.SOUTH);
 		
+		title = new TitleBar();
+		footer = new Footer();
+		list = new List();
+		
+		this.add(title,BorderLayout.NORTH);
+		this.add(footer,BorderLayout.SOUTH);
 		this.add(list,BorderLayout.CENTER);
 		
-		addTask = btnPanel.getAddTask();
-		clear = btnPanel.getClear();
+		newTask = footer.getNewTask();
+		clear = footer.getClear();
 		
-		addListners();
-		
-	} 
-
-
+		addListeners();
+	}
 	
-	public void addListners()
+	
+	public void addListeners()
 	{
-		addTask.addMouseListener(new MouseAdapter()
+		newTask.addMouseListener(new MouseAdapter()
 		{
-			
+			@override
 			public void mousePressed(MouseEvent e)
 			{
 				Task task = new Task();
 				list.add(task);
 				list.updateNumbers();
 				
+				
 				task.getDone().addMouseListener(new MouseAdapter()
-			{
-			    public void mousePressed(MouseEvent e)
-			    {
-			    	task.changeState();
-			    	revalidate();
-			    }
-	
+				{
+					@override
+					public void mousePressed(MouseEvent e)
+					{
+						
+						task.changeState();
+						list.updateNumbers();
+						revalidate();
+						
+					}
+				});
+			}
+			
 		});
-				revalidate();
+		
+		
+		clear.addMouseListener(new MouseAdapter()
+		{
+			@override
+			public void mousePressed(MouseEvent e)
+			{
+				list.removeCompletedTasks();
+				repaint();
 			}
 		});
-	}
-}
-
+	}	
 	
-
-
+}
